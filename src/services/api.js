173e11f28a391cf3216e1902def2277f5361d895
Vladimir Avakumov <avakdev@gmail.com>
2020-12-10 18:20:06 +0400

    api refactor

--- src/services/api.js
+++ src/services/api.js
@@ -1,53 +1,52 @@
-import {postData, deleteData} from '../helpers/utils'
+import { postData, deleteData } from "../helpers/utils"
 
-const getPosts= (page) => {
-  return fetch(`http://localhost:3001/api/post/${page}`)
-.then(response => response.json())
-.then(data => data.data)
+const URL = "http://localhost:3001/api"
+
+const getPosts = (page) => {
+  return fetch(`${URL}/post/${page}`)
+    .then((response) => response.json())
+    .then((data) => data.data)
 }
 
-const getTags= () => {
-  return fetch(`http://localhost:3001/api/tag/`)
-.then(response => response.json())
-.then(data => data.data)
+const getTags = () => {
+  return fetch(`${URL}/tag/`)
+    .then((response) => response.json())
+    .then((data) => data.data)
 }
 
-const getNotes= (filter={}) => {
-  let endURL = ''
-  const {tagId, _id} = filter
+const getNotes = (filter = {}) => {
+  let endURL = ""
+  const { tagId, _id } = filter
   if (tagId) {
     endURL = `byTag/${tagId}`
-  } 
-  if (_id) {
-    endURL=`${_id}`
   }
-  return fetch(`http://localhost:3001/api/note/${endURL}`)
-.then(response => response.json())
-.then(data => data.data)
+  if (_id) {
+    endURL = `${_id}`
+  }
+  return fetch(`${URL}/note/${endURL}`)
+    .then((response) => response.json())
+    .then((data) => data.data)
 }
 
 const deletePost = (id) => {
-  return deleteData(`http://localhost:3001/api/note/${id}`)
-.then(response => response.json())
-.then(data => data.data)
+  return deleteData(`${URL}/note/${id}`)
+    .then((response) => response.json())
+    .then((data) => data.data)
 }
 
 const postTag = (tagName) => {
-  return postData("http://localhost:3001/api/tag", {name: tagName})
-    .then((data) => data.json())
+  return postData(`${URL}/tag`, { name: tagName }).then((data) => data.json())
 }
 
 const postNote = (note) => {
-  return postData("http://localhost:3001/api/note", note)
-    .then((data) => data.json())
+  return postData(`${URL}/note`, note).then((data) => data.json())
 }
 
-
 export const api = {
-    getPosts,
-    getTags,
-    getNotes,
-    postTag,
-    postNote,
-    deletePost
+  getPosts,
+  getTags,
+  getNotes,
+  postTag,
+  postNote,
+  deletePost,
 }
